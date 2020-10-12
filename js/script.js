094c03b8d35fc42918d715d2c97cebf3474e6e41
noiseLim <noiselim@gmail.com>
2020-10-12 22:43:09 +0300

    add async/await script

--- js/script.js
+++ js/script.js
@@ -222,118 +222,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Forms
 
-    // const forms = document.querySelectorAll('form');
-
-    // const message = {
-    //     // loading: 'Загрузка',
-    //     loading: 'img/form/spinner.svg',
-    //     success: 'Спасибо! Скоро мы с вами свяжемся!',
-    //     failure: 'Что-то пошло не так'
-    // };
-
-    // forms.forEach(item => {
-    //     postData(item);
-    // });
-
-    // function postData(form) {
-    //     form.addEventListener('submit', (e) => {
-    //         e.preventDefault();
-
-    //         // const statusMessage = document.createElement('div');
-    //         // statusMessage.classList.add('status');
-    //         // statusMessage.textContent = message.loading;
-    //         // form.append(statusMessage);
-
-    //         const statusMessage = document.createElement('img');
-    //         statusMessage.src = message.loading;
-    //         statusMessage.style.cssText = `
-    //             display: block;
-    //             margin: 0 auto;
-    //         `;
-
-    //         // form.append(statusMessage);
-    //         // Чтобы спинер стоял аккуратней
-    //         form.insertAdjacentElement('afterend', statusMessage);
-
-
-    //         const request = new XMLHttpRequest();
-    //         request.open('POST', 'server.php');
-
-    //         // при использовании formData использовать заголовок не нужно
-    //         // request.setRequestHeader('Content-type', 'multipart/form-data');
-            
-
-    //         //используем JSON
-    //         request.setRequestHeader('Content-type', 'application/json');
-    //         const formData = new FormData(form);
-
-    //         /*прогоним FormData при помощи перебора forEach 
-    //         и получим обычный объект*/
-    //         const object = {};
-    //         formData.forEach(function(value, key) {
-    //             object[key] = value;
-    //         });
-
-    //         //теперь уже обычный объект преобразуем в JSON
-    //         const json = JSON.stringify(object);
-    //         request.send(json);
-
-
-
-    //         // request.send(formData);
-
-    //         request.addEventListener('load', () => {
-    //             if (request.status === 200) {
-    //                 console.log(request.response);
-    //                 // statusMessage.textContent = message.success;
-    //                 showThanksModal(message.success);
-    //                 form.reset();
-    //                 // setTimeout(() => {
-    //                 //     statusMessage.remove();
-    //                 // }, 2000);
-    //                 statusMessage.remove();
-    //             } else {
-    //                 // statusMessage.textContent = message.failure;
-    //                 showThanksModal(message.failure);
-    //             }
-    //         });
-
-    //     });
-    // }
-
-    // // Modify modal
-
-    // function showThanksModal(message) {
-    //     const prevModalDialog = document.querySelector('.modal__dialog');
-
-    //     prevModalDialog.classList.add('hide');
-    //     openModal();
-
-    //     const thanksModal = document.createElement('div');
-    //     thanksModal.classList.add('modal__dialog');
-    //     thanksModal.innerHTML = `
-    //         <div class="modal__content">
-    //             <div class="modal__close" data-close>×</div>
-    //             <div class="modal__title">${message}</div>
-    //         </div>
-    //     `;
-    //     document.querySelector('.modal').append(thanksModal);
-    //     setTimeout(() => {
-    //         thanksModal.remove();
-    //         prevModalDialog.classList.add('show');
-    //         prevModalDialog.classList.remove('hide');
-    //         closeModal();
-    //     }, 10000);
-
-    // }
-
-
-/////////////////////////////////////////////////////
-
-
-
-
-
     const forms = document.querySelectorAll('form');
 
     const message = {
@@ -343,31 +231,44 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindpostData(item);
     });
 
-    function postData(form) {
+    /*async указывает, что внутри функции будет асинхронный код
+    await (парный оператор) препятствует асинхронности кода, благодаря 
+    нему переменная res дождется окончания запроса, присвоит его и 
+    тогда код пойдет дальше*/
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
+
+    function bindpostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
+
             const statusMessage = document.createElement('img');
             statusMessage.src = message.loading;
             statusMessage.style.cssText = `
                 display: block;
                 margin: 0 auto;
             `;
-            
             // Чтобы спинер стоял аккуратней
             form.insertAdjacentElement('afterend', statusMessage);
-
 
             // const request = new XMLHttpRequest();
             // request.open('POST', 'server.php');
 
-
             // при использовании formData использовать заголовок не нужно
             // request.setRequestHeader('Content-type', 'multipart/form-data');
             
-
             //используем JSON
             // request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
@@ -379,15 +280,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-
             // request.send(json);
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            }).then(data => data.text())
+            postData('http://localhost:3000/requests', JSON.stringify(object))
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
